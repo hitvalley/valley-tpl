4b99d0a40b0e1a373a7f691d9f48e759844ce423
gty130623 <gty130623@alibaba-inc.com>
2017-08-29 15:33:17 +0800

    update package.json

---
+++ src/node-plus.js
@@ -0,0 +1,8 @@
+import vtpl from './node';
+import datestr from './plugins/datestr';
+import htmlspecialchars from './plugins/htmlspecialchars';
+
+vtpl.register('datestr', datestr);
+vtpl.register('htmlspecialchars', htmlspecialchars);
+
+export default vtpl;
